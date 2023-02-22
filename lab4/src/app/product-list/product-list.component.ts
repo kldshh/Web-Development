465fd3a507eeb1e04d4890311e6ff144a850703c
kldshh <98958005+kldshh@users.noreply.github.com>
2023-02-22 22:47:51 +0600

    Add files via upload

---
+++ lab4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share(name:string, url:string) {
+    window.open(`https://telegram.me/share/url?url=Welcome, you should buy ${name} &text= on ${url}`)
+  } 
+}
